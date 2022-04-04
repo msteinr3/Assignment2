738f1183af414a85cfdfe6fd655f5d6c2f7c770c
mayasteiner1 <mayasparkin1@gmail.com>
2022-04-04 10:56:19 -0500

    working location

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.assignment2
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.*
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.assignment2.databinding.ActivityMainBinding
@@ -16,93 +16,51 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.Task
 import java.util.*
 
-class MainActivity : AppCompatActivity(), LocationListener {
+open class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private var permissionID = 1000
-
-    private lateinit var locationManager: LocationManager
-    private var currentLocation: Location? = null
-    private lateinit var lat: String
-    private lateinit var long: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         binding.getLocation.setOnClickListener {
-            //fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
             getLocation()
         }
+
 
         binding.sendLocation.setOnClickListener {
 
         }
     }
 
-
     private fun getLocation() {
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionID)
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
-    }
-    override fun onLocationChanged(location: Location) {
-        lat = location.latitude.toString()
-        long = location.longitude.toString()
-        val txt = findViewById<TextView>(R.id.address)
-        txt.text = "$lat ; $long"
-    }
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == permissionID) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+        val task: Task<Location> = fusedLocationProviderClient.lastLocation
+        val geocoder = Geocoder(this, Locale.getDefault())
+        var addresses:List<Address>
 
-
-/*
-
-    private fun getLocation() : String {
-        val task : Task<Location> = fusedLocationProviderClient.lastLocation
-        var answer = ""
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED)
+            && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
             != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), permissionID)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
         }
-
-        //var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        //return locationManager.isProviderEnabled((LocationManager.GPS_PROVIDER)
-
-
-        if (task.isSuccessful && task.result != null) {
-            answer = task.result.latitude.toString() + " ; " + task.result.longitude.toString()
-            return  answer
-        } else {
-            return "No Location Detected"
+        task.addOnSuccessListener {
+            if(it != null){
+                addresses = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+                val lat = findViewById<TextView>(R.id.latitude)
+                val long = findViewById<TextView>(R.id.longitude)
+                val address = findViewById<TextView>(R.id.address)
+                lat.text = it.latitude.toString()
+                long.text = it.longitude.toString()
+                address.text = addresses[0].getAddressLine(0)
+            }
         }
     }
-
-    private fun getAddress(lat: Double, long: Double) : String {
-        var city = ""
-        var State = ""
-        var geoCoder = Geocoder(this, Locale.getDefault())
-        var Address : (Mutable)List<Address!>! = geoCoder.getFromLocation(lat, long, 1)
-
-        city = Address.get(0).locality
-        return city
-    }
-*/
 
 }
